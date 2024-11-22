630de052fe9cf232a4b36829b0628213b125d14f
Abhay <zzxxcc6386@gmail.com>
2024-11-22 13:15:22 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Node.js Docker app!' });
+  res.json({ message: 'Hello from Node.js Docker app! check update abhay' });
 });
 
 app.listen(port, () => {
